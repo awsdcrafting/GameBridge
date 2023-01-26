013fd004e567f403254f0e2ed19ae53c7cc6fcfd
scisneromam <scisneromam@t-online.de>
2023-01-26 15:13:12 +0100

    factorio

--- factorio/gamebridge/control.lua
+++ factorio/gamebridge/control.lua
@@ -1,11 +1,14 @@
-commands.add_command('scis_airbridge.add', 'adds items into a chest', function(command)
+commands.add_command('scis_gamebridge.add', 'adds items into a chest', function(command)
     if command.player_index then
         return
     end
-    rcon.print("hi")
+    if not command.parameter then
+        return
+    end
+    game.json_to_table(command.parameter)
 end)
 
-commands.add_command('scis_airbridge.get', 'Gets all players', function(command)
+commands.add_command('scis_gamebridge.get', 'Gets all items indexed by chest', function(command)
     if command.player_index then
         return
     end
